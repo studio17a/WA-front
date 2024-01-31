61a2a201ff78b52d9f26389bd92234838783d233
studio 17A <88838883+studio17a@users.noreply.github.com>
2024-01-31 08:12:25 +0100

    Update apiSlice.js

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://ktmjn2-3500.csb.app/",
+  baseUrl: "https://warsztap-api.onrender.com/",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
