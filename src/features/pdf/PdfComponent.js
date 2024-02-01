b48de5c528a031d5920206e12c7d546c0eef0bbf
STUDIO 17A <88838883+studio17a@users.noreply.github.com>
2024-02-01 09:07:36 +0000

    feat: Update import statements, add MyDoc component and update Button component

--- src/features/pdf/PdfComponent.js
+++ src/features/pdf/PdfComponent.js
@@ -1,27 +1,8 @@
 import { Button } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
-import {
-  PDFDownloadLink,
-  Document,
-  Page,
-  Text,
-  View,
-} from "@react-pdf/renderer";
-const MyDoc = ({ appointment }) => (
-  <Document>
-    {/*render a single page*/}
-    <Page size="A4">
-      <View>
-        <Text>klient: {appointment.user.username}</Text>
-      </View>
-      <View>
-        <Text>data: {appointment.date}</Text>
-        <Text>pojazd: {appointment.vehicle.reg}</Text>
-      </View>
-    </Page>
-  </Document>
-);
+import MyDoc from "./MyDoc";
+import { PDFDownloadLink } from "@react-pdf/renderer";
 
 function PdfComponent({ appointment }) {
   return (
@@ -32,11 +13,11 @@ function PdfComponent({ appointment }) {
       >
         {({ blob, url, loading, error }) =>
           loading ? (
-            <Button isDisabled={true}>
+            <Button bg="transparent" isDisabled={true}>
               <FontAwesomeIcon color="#30bccf" size="xl" icon={faFilePdf} />
             </Button>
           ) : (
-            <Button>
+            <Button bg="transparent">
               <FontAwesomeIcon color="#30bccf" size="xl" icon={faFilePdf} />
             </Button>
           )
