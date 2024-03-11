9bbf22d73a3f5eeaafaf4396a2a1d445c2f2e92e
marek walczak <walczak.m@icloud.com>
2024-03-11 13:16:03 +0000

    d

--- src/components/DashLayout.js
+++ src/components/DashLayout.js
@@ -2,26 +2,31 @@ import { Outlet } from "react-router-dom";
 import DashHeader from "./DashHeader";
 import DashFooter from "./DashFooter";
 import { VStack, Container, Box } from "@chakra-ui/react";
+import useAuth from "../hooks/useAuth";
 
 const DashLayout = () => {
-  return (
-    // <VStack h="100vh" w="100%" backgroundColor="#14b1d9">
-    <VStack w="100%" backgroundColor="#edf9ff">
-      <DashHeader />
-      <Box m={0} p={4} w="100%">
-        <Box
-          bg="#fff"
-          border="1px solid #eee"
-          borderRadius="10px"
-          m={0}
-          padding="20px"
-          w="100%"
-        >
-          <Outlet w="100%" />
+  const { username, isManager, isAdmin } = useAuth();
+  let dashContent = <></>;
+  if (isAdmin) {
+    dashContent = (
+      <VStack w="100%" backgroundColor="#edf9ff">
+        <DashHeader />
+        <Box m={0} p={4} w="100%">
+          <Box
+            bg="#fff"
+            border="1px solid #eee"
+            borderRadius="10px"
+            m={0}
+            padding="20px"
+            w="100%"
+          >
+            <Outlet w="100%" />
+          </Box>
         </Box>
-      </Box>
-      {/* <DashFooter /> */}
-    </VStack>
-  );
+        {/* <DashFooter /> */}
+      </VStack>
+    );
+  }
+  return dashContent;
 };
 export default DashLayout;
