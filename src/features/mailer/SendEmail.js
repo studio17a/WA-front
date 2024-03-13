98a0ad793fda3e6daef0b49f0cf280246ec0ce1e
marek walczak <walczak.m@icloud.com>
2024-03-13 09:06:52 +0000

    mailer

--- src/features/mailer/SendEmail.js
+++ src/features/mailer/SendEmail.js
@@ -3,7 +3,9 @@ import { useEffect } from "react";
 import { Spinner } from "@chakra-ui/react";
 
 const SendEmail = ({ task, body }) => {
-  let message = "";
+  let message = <Spinner />;
+  console.log(task);
+  console.log("SENDING EMAIL file");
   const [send, { isLoading, isSuccess, isError, error }] =
     useSendEmailMutation();
   // console.log(data);
@@ -15,12 +17,15 @@ const SendEmail = ({ task, body }) => {
   }, []);
   if (isLoading) message = <Spinner />;
   if (isSuccess) {
+    console.log("success SENDING EMAIL");
     if (task === "new")
       message = `Aby potwierdzić, proszę kliknąć na link wysłany na adres: ${body.email}`;
     // if (task === "delete") message = "E-mail został usunięty";
   }
   if (isError) {
+    console.log("error SENDING EMAIL");
     console.log(error);
   }
+  return message;
 };
 export default SendEmail;
